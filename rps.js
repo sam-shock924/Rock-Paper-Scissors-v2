6e70aa692df328432e1689ed34d84058771319c3
Sam Shock <sam.shock924@gmail.com>
2021-10-12 15:34:00 -0400

    button press changes text

--- rps.js
+++ rps.js
@@ -1,1 +1,26 @@
-console.log('sup')
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const gameOutcome = document.getElementById('gameOutcome');
+
+
+rockButton.addEventListener('click', () => {
+    gameOutcome.innerText = "Rock button pressed!"
+})
+
+
+
+paperButton.addEventListener('click', () => {
+    gameOutcome.innerText = "Paper button pressed!"
+})
+
+
+
+
+scissorsButton.addEventListener('click', () => {
+    gameOutcome.innerText = "Scissors button pressed!"
+})
+//lose function
+
+//outcome
+
